d4cf96804b3cdb26703fba9446b654a57433773a
usuario01 <usuario01@gmail.com>
2021-04-06 21:01:00 -0500

    Update Client class

--- src/pe/edu/tecsup/app/entities/Cliente.java
+++ src/pe/edu/tecsup/app/entities/Cliente.java
@@ -7,9 +7,48 @@ public class Cliente {
 	private String paterno;
 	private String materno;
 
+	public Cliente(int codigo, String nombres, String paterno, String materno) {
+		super();
+		this.codigo = codigo;
+		this.nombres = nombres;
+		this.paterno = paterno;
+		this.materno = materno;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombres() {
+		return nombres;
+	}
+
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+
+	public String getPaterno() {
+		return paterno;
+	}
+
+	public void setPaterno(String paterno) {
+		this.paterno = paterno;
+	}
+
+	public String getMaterno() {
+		return materno;
+	}
+
+	public void setMaterno(String materno) {
+		this.materno = materno;
+	}
+
 	public String getNombreCompleto() {
 		return this.nombres + " " + this.paterno + " " + this.materno;
 	}
 
-	
 }
